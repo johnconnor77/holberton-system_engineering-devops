6f63a3aa63242f281e5186a44d61e0a7d34f7276
johnconnor77 <jfcalleh@unal.edu.co>
2019-12-10 17:10:41 -0500

    Puppet to make changes to our configuration

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Create changes with puppet
+file_line {'Turn off passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  ensure => 'present',
+  line   => 'ForwardAgent yes '
+}
+
+file_line {'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  ensure => 'present',
+  line   => 'identityFile ~/.ssh/holberton'
+}
+
+
