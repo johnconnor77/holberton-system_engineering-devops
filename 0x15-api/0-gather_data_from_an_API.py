f7f965795fa0fcd9cadde648ca110616957483b7
johnconnor77 <jfcalleh@unal.edu.co>
2020-05-04 23:05:29 -0500

    Python script that returns information about a peron's To Do list when the employee ID is given

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    Python script that returns information about a persons
+    To Do list when the employee ID is given
+"""
+
+import requests
+from sys import argv
+
+
+def get_todolist_progress():
+    """
+    Get's the  to do list progress of a specified user
+    """
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+
+    user_info = requests.get(url + "users/{}"
+                             .format(user_id)).json()
+    total_user_tasks = requests.get(url + "todos?userId={}"
+                                    .format(user_id)).json()
+    completed_user_tasks = requests.get(url + "todos?userId={}&&completed=true"
+                                        .format(user_id)).json()
+
+    try:
+        print("Employee {} is done with tasks({}/{}):"
+              .format(user_info.get('name'),
+                      len(completed_user_tasks), len(total_user_tasks)))
+
+        for task in completed_user_tasks:
+            print("\t{}".format(task.get("title")))
+    except KeyError:
+        pass
+
+
+if __name__ == "__main__":
+    get_todolist_progress()
