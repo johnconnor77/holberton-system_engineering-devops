63a4f297f840604a3ad13474c0da362000f57f89
johnconnor77 <jfcalleh@unal.edu.co>
2020-05-08 00:14:15 -0500

    Raise another except when Attribute it's not being setted as expected

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,16 +10,19 @@ def top_ten(subreddit):
         Print the titles of the first 10 hot posts listed for a given subreddit
     """
 
-    url = "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'user-agent': 'Top Ten'}
-    req = requests.get(url, headers=headers, allow_redirects=False)
-    top_json_all = req.json()
-    top_json = top_json_all.get('data').get('children')
+    try:
+        url = "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+        headers = {'user-agent': 'Top Ten'}
+        req = requests.get(url, headers=headers, allow_redirects=False)
+        top_json = req.json().get('data').get('children')
 
-    if req.status_code == 200:
-        for top in top_json:
-            print(top.get('data').get('title'))
-    else:
+        if req.status_code == 200:
+            for top in top_json:
+                print(top.get('data').get('title'))
+
+    except KeyError:
+        print(None)
+    except AttributeError:
         print(None)
 
 
